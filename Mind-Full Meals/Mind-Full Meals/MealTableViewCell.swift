fce43ed57ebf856b5b296470da35704c83da722a
mwa96 <mwa96@asb9700-e05.csil.sfu.ca>
2018-07-13 19:42:22 -0700

    No longer crashes

--- Mind-Full Meals/Mind-Full Meals/MealTableViewCell.swift
+++ Mind-Full Meals/Mind-Full Meals/MealTableViewCell.swift
@@ -13,11 +13,21 @@ import UIKit
 class MealTableViewCell: UITableViewCell {
 
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
     @IBOutlet weak var ingredientsLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var beforeHunger: UILabel!
+    @IBOutlet weak var afterHunger: UILabel!
 
     func setMeal(meal: Meal) {
         nameLabel.text = meal.GetMealName()
+        ratingLabel.text = String(meal.GetRating())
         ingredientsLabel.text = meal.GetIngredients().description
+        dateLabel.text = dateToString(mealDate: meal.GetDate())
+        typeLabel.text = meal.GetMeal_Type()
+        beforeHunger.text = meal.GetBefore()
+        afterHunger.text = meal.GetAfter()
     }
     
     override func awakeFromNib() {
@@ -31,4 +41,12 @@ class MealTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    private func dateToString(mealDate: Date) -> String {
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = DateFormatter.Style.medium
+        dateFormatter.timeStyle = DateFormatter.Style.none // No time is shown
+        
+        return dateFormatter.string(from: mealDate)
+    }
 }
