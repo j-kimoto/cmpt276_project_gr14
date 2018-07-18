cbb974d8c6ba223e40ddc828a489c1aefb1021ad
jbengco <40783306+jbengco@users.noreply.github.com>
2018-07-17 21:31:34 -0700

    Progress on weekly calendar database

--- Mind-Full Meals/Mind-Full Meals/WeekViewController.swift
+++ Mind-Full Meals/Mind-Full Meals/WeekViewController.swift
@@ -24,6 +24,10 @@ class WeekViewController: UIViewController, UITableViewDelegate,UITableViewDataS
     
     @IBOutlet weak var WeekTableView: UITableView!
     var db: OpaquePointer?
+    var counter = 0
+    var NameData = [String]()
+    var DateData = [Int32]()
+    var TypeData = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,6 +42,34 @@ class WeekViewController: UIViewController, UITableViewDelegate,UITableViewDataS
         else {
             print("Connected to database")
         }
+        
+        var stmt: OpaquePointer?
+        let queryString = "SELECT Name, Date, Type from Meals WHERE Date BETWEEN ? AND ?"
+        
+        // Preparing the query for database search
+        if sqlite3_prepare_v2(db, queryString, -1, &stmt, nil) != SQLITE_OK {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("Error preparing insert: \(errmsg)")
+        }
+        
+        // Binding the parameters and throwing error if not ok
+        if sqlite3_bind_int(stmt, 1, CalcTime()) != SQLITE_OK {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("Error binding start date: \(errmsg)")
+        }
+        if sqlite3_bind_int(stmt, 2, CalcTime()+604800) != SQLITE_OK {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("Error binding end date: \(errmsg)")
+        }
+        
+        while (sqlite3_step(stmt) == SQLITE_ROW) {
+            let resultscol0 = sqlite3_column_text(stmt, 0)
+            NameData[counter] = String(cString: resultscol0!)
+            DateData[counter] = sqlite3_column_int(stmt, 1)
+            let resultscol2 = sqlite3_column_text(stmt,2)
+            TypeData[counter] = String(cString: resultscol2!)
+            counter += 1
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,14 +77,23 @@ class WeekViewController: UIViewController, UITableViewDelegate,UITableViewDataS
         // Dispose of any resources that can be recreated.
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return counter
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! UITableViewCell
+        return cell
     }
 
     override func viewDidAppear(_ animated: Bool) {
         WeekTableView.reloadData()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Close the database when switching views
+        sqlite3_close(db)
+        print("Closed the database")
     }
     /*
     // MARK: - Navigation
