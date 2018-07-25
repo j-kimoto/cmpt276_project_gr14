a4d76f748d8d7df0348c9d7191afbf7caaca73fe
jbengco <40783306+jbengco@users.noreply.github.com>
2018-07-24 18:18:48 -0700

    Fixed add meal view controller
    Works with user saved dates, also some navigation updates

--- Mind-Full Meals/Mind-Full Meals/AddMealViewController.swift
+++ Mind-Full Meals/Mind-Full Meals/AddMealViewController.swift
@@ -12,33 +12,50 @@ class AddMealViewController: UIViewController {
     
     @IBAction func breakfastButton(_ sender: Any) {
         UserDefaults.standard.set(0, forKey:"udtype") // Int
-        //let bTime = UserDefaults.standard.object(forKey: "DBT") as? Date ?? Date()
-        //UserDefaults.standard.set(setDatePicker(arg1: bTime), forKey:"uddate") // Date is stored as Any object
-        UserDefaults.standard.set(setDatePicker(arg1: "2018-07-20 07:00"), forKey:"uddate") // Date is stored as Any object
+        let bTime = UserDefaults.standard.object(forKey: "DBT") as? Date ?? Date()
+        if saved {
+            print("BREAKFAST TIME:", bTime)
+            UserDefaults.standard.set(bTime, forKey:"uddate") // Date is stored as Any object
+        }
+        else {
+            print("UNSAVED TIME")
+            UserDefaults.standard.set(setDateTime(hour: 7, min: 0), forKey:"uddate") // Date is stored as Any object
+        }
         
     }
     
     @IBAction func lunchButton(_ sender: Any) {
         UserDefaults.standard.set(1, forKey:"udtype") // Int
-        //let lTime = UserDefaults.standard.object(forKey: "DLT") as? Date ?? Date()
-        UserDefaults.standard.set(setDatePicker(arg1: "2018-07-20 13:00"), forKey:"uddate") // Date is stored as Any object
+        let lTime = UserDefaults.standard.object(forKey: "DBT") as? Date ?? Date()
+        if saved {
+            print("LUNCH TIME:", lTime)
+            UserDefaults.standard.set(lTime, forKey:"uddate") // Date is stored as Any object
+        }
+        else {
+            print("UNSAVED TIME")
+            UserDefaults.standard.set(setDateTime(hour: 13, min: 0), forKey:"uddate") // Date is stored as Any object
+        }
     }
     
     @IBAction func dinnerButton(_ sender: Any) {
         UserDefaults.standard.set(2, forKey:"udtype") // Int
-        //let dTime = UserDefaults.standard.object(forKey: "DDT") as? Date ?? Date()
-        UserDefaults.standard.set(setDatePicker(arg1: "2018-07-20 18:00"), forKey:"uddate") // Date is stored as Any object
+        let dTime = UserDefaults.standard.object(forKey: "DBT") as? Date ?? Date()
+        if saved {
+            print("DINNER TIME:", dTime)
+            UserDefaults.standard.set(dTime, forKey:"uddate") // Date is stored as Any object
+        }
+        else {
+            print("UNSAVED TIME")
+            UserDefaults.standard.set(setDateTime(hour: 18, min: 0), forKey:"uddate") // Date is stored as Any object
+        }
     }
     
     @IBAction func snackButton(_ sender: Any) {
         UserDefaults.standard.set(3, forKey:"udtype") // Int
     }
-    
-    private func setDatePicker(arg1: String) -> Date {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
-        //dateFormatter.dateFormat = "HH:mm"
-        let time = dateFormatter.date(from:arg1)
-        return time!
+
+    private func setDateTime(hour: Int, min: Int) -> Date {
+        let date = Calendar.current.date(bySettingHour: hour, minute: min, second: 0, of: Date())!
+        return date
     }
 }
